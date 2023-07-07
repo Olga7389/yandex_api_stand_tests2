086a9dead4d98cf34101d4b108c6573fbdaa94a3
Olga7389 <olga7389@mail.ru>
2023-07-07 11:15:04 +0300

    first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://8204114f-1f44-42c2-9f72-71694003df84.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
